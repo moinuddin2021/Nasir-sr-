5fc68a9a0f312c508b9b4d8d36ca7a5c50c7ea65
Shad <shadrajput6@gmail.com>
2022-08-20 11:17:18 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import Sidebar from "./Componant/sidebar"
 import Searchbar from './Componant/searchbar';
+import Main from './Componant/content';
 
 function App() {
   return (
     <div className="App bg-Shad-400 min-h-screen flex">
           
             <Sidebar/>
+            <div className='w-full'>
             <Searchbar/>
+            <Main/>
+
+            </div>
           
     </div>
   );
